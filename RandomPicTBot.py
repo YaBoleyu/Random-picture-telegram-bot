0df6c1fa617a6648433c19451e64aa34eb2c0575
YaBoleyu <35567310+YaBoleyu@users.noreply.github.com>
2020-06-08 17:16:03 +0300

    Update RandomPicTBot.py

--- RandomPicTBot.py
+++ RandomPicTBot.py
@@ -4,21 +4,13 @@ import os
 
 path = "PATH"
 
-
+# Random picture function 
 def init(bot, update):
     directory = path
     random_image = random.choice(os.listdir(directory))
     bot.send_photo(update.message.chat_id, photo=open(
         directory + random_image, 'rb'))
-
-
-def Desktop(bot, update):
-    directory = "PATH"
-    random_image = random.choice(os.listdir(directory))
-    bot.send_photo(update.message.chat_id, photo=open(
-        directory + random_image, 'rb'))
-
-
+    
 def main():
     updater = Updater('TOKEN')
     dp = updater.dispatcher
